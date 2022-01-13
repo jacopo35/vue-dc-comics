626aa0288e8d0828adb7a87d6d90e988b9516ef9
jacopo35 <jaczor00@gmail.com>
2022-01-13 21:05:39 +0100

    add-Header-Main-Footer in App.vue

---
+++ public/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import Header from '../src/components/Header.vue';
+import Main from '../src/components/Main.vue';
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/scss/Style.scss"
+</style>
---
+++ public/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+      <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header', 
+    daa() {
+        return {
+            title: ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
